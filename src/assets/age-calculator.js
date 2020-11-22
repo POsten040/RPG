2326829a251becae831706f3967c41af2c7fa344
Patrick Osten <Patrickosten040@gmail.com>
2020-11-22 11:30:58 -0800

    refactor age calculation functions into one function

--- src/assets/age-calculator.js
+++ src/assets/age-calculator.js
@@ -3,45 +3,24 @@ export default class AgeCalculator {
     this.name = name;
     this.age = Math.abs(age);
     this.relativeAge;
-    this.planet = planet
+    this.expec = 80;
+    this.planet = planet;
     this.planetWeight = new Map([
       ['Earth', 1],
       ['Mercury', 0.24],
       ['Venus', 0.62],
       ['Mars', 1.88],
       ['Jupiter', 11.6]
-    ])
-}
+    ]);
+  }
   planetAge() {
     let weight = this.planetWeight.get(this.planet);
-    this.relativeAge = Math.floor(parseInt(this.age) * weight)
-    console.log(`${this.name} is ${this.relativeAge} years old on ${this.planet}`)
+    this.relativeAge = Math.floor(parseInt(this.age) * weight);
+    let expected = (this.expec - this.age) * weight;
+    if (Math.sign(expected) === 1) { 
+      return(`${this.name} is ${this.relativeAge} years old on ${this.planet}, and they have ${expected} ${this.planet} years left to be groovy.`);
+    } else {
+      return (`${this.name} is ${this.relativeAge} years old on ${this.planet}, and they have been groovy ${Math.abs(expected)} ${this.planet} years more than we expected.`);
+    }
   }
-  mercuryAge() {
-    this.relativeAge = Math.floor(parseInt(this.age) * this.planet.get['Earth']);
-    return (`${this.name} is ${this.relativeAge} years old on Mercury!`);
-  }
-  venusAge() {
-    this.relativeAge = Math.floor(parseInt(this.age) * this.planet.get['Mercury']);
-    return (`${this.name} is ${this.relativeAge} years old on Venus!`);
-  }
-  marsAge() {
-    this.relativeAge = Math.floor(parseInt(this.age) * this.planet.get['Venus']);
-    return (`${this.name} is ${this.relativeAge} years old on Mars!`);
-  }
-  jupiterAge() {
-    this.relativeAge = Math.floor(parseInt(this.age) * this.planet.get['Mars']);
-    return (`${this.name} is ${this.relativeAge} years old on Jupiter!`);
-  }
-  // expectancy() {
-  //   let planets = this.planet;
-  //   for (let [key, value] of Object.entries(planets)) {
-  //     let this.age * value;
-  //   }
-  //     return `Here's your life expectancy on different planets: You've lived ${eAge} years past your expected demise on Earth, ${meAge} years past your expected demise on Mercury, ${vAge} years past your expected demise on Venus, ${maAge} years past your expected demise on Mars, and ${jAge} years past your expected demise on Jupiter!`;
-  //   }
-  //   else {
-  //     return `Here's your life expectancy on different planets: You have ${eAge} years left on Earth, ${meAge} years left on Mercury, ${vAge} years left on Venus, ${maAge} years left on Mars, and ${jAge} years left on Jupiter!`;
-  //   }
-  // }
 }
--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ import "./assets/css/styles.css";
 import $ from "jquery";
 import AgeCalculator from "./assets/age-calculator.js";
 
+
 $("#user-input").submit(function(event) {
   event.preventDefault();
   let inputName = $("#user-name").val();
   let inputAge = $("#user-date").val();
   let inputPlanet = $("input[type=radio]:checked").val();
-  console.log(inputPlanet);
   let user = new AgeCalculator(inputName, inputAge, inputPlanet)
   $("#results").html("<span>" + user.planetAge() + "</span>");
+  $("#results").html("<span>" + user.expectancy() + "</span>");
+  $("#reset").toggle();
 });
