040d90cd735aacd2b74f0f3166bdc2716513a6af
Patrick <posten.coding@gmail.com>
2021-02-03 09:48:04 -0800

    create initial directories
    Co-authored-by: Garrett Brown <gman9mm@live.com>
    Co-authored-by: Nataliya Zhuravleva<natalindria@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,17 +1,1 @@
-import "bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./assets/css/styles.css";
-import $ from "jquery";
-import AgeCalculator from "./assets/age-calculator.js";
 
-
-$("#user-input").submit(function(event) {
-  event.preventDefault();
-  let inputName = $("#user-name").val();
-  let inputAge = $("#user-date").val();
-  let inputPlanet = $("input[type=radio]:checked").val();
-  let user = new AgeCalculator(inputName, inputAge, inputPlanet)
-  $("#results").html("<span>" + user.planetAge() + "</span>");
-  $("#results").html("<span>" + user.expectancy() + "</span>");
-  $("#reset").toggle();
-});
